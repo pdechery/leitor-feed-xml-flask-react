b137e434ce9e2413e2967ffbe35b059834787257
Pierre Dechery <pierre.dechery@gmail.com>
2020-04-03 16:16:01 -0300

    Working

--- htmlparser.py
+++ htmlparser.py
@@ -7,43 +7,54 @@ class parseItemContent(HTMLParser):
     def __init__(self):
         HTMLParser.__init__(self)
         
-        self.ps, self.imgs, self.uls = [], [], []
-        self.getP, self.getUl, self.getImg = 0, 0, 0
-
-        self.data = {
-            'ps': self.ps,
-            'imgs': self.imgs,
-            'uls': self.uls
-        }
+        self.getData, self.isUl, self.isDiv = False, False, False
+        self.paragraph, self.links, self.content = [], [], []
 
     def handle_starttag(self, tag, attrs):
         if (tag == 'p'):
-            self.ps.append('<p>')
-            self.getP = 1
-        elif(tag == 'img'):
+            self.getData = True
+        elif(tag == 'div'):
+            self.isDiv = True
+        elif(tag == 'img' and self.isDiv):
             for attr in attrs:
                 if(attr[0] == 'src'):
-                    self.imgs.append(attr[1])
-        elif(tag == 'li'):
-            self.getUl = 1
-        elif(tag == 'a'):
-            if(self.getUl):
-                self.uls.append(attrs[0][1])
+                    self.content.append({
+                        'type': 'image',
+                        'content': attr[1]
+                    })
+        elif(tag == 'ul'):
+            self.isUl = True
+        elif(tag == 'a' and self.isUl):
+            self.links.append(attrs[0][1])
         else:
             return
 
     def handle_endtag(self, tag):
         if (tag == 'p'):
-            self.ps.append('</p>')
-            self.getP = 0
-        elif(tag == 'li'):
-            self.getUl = 0
+            self.getData = False
+            text = ' '.join(self.paragraph)
+            if(text):
+                text = text.replace(" ,", ",")
+                self.content.append({
+                    'type':'text',
+                    'content': text
+                })
+            self.paragraph = []
+        elif(tag == 'ul'):
+            self.isUl = False
+            self.content.append({
+                'type':'links',
+                'content': self.links
+            })
+        elif(tag == 'div'):
+            self.isDiv = False
         else:
             return
 
     def handle_data(self, data):
-        if(self.getP):
+        if(self.getData):
             nospace = data.strip()
-            self.ps.append(nospace)
+            if(nospace):
+                self.paragraph.append(nospace)
         else:
             return
--- run.py
+++ run.py
@@ -5,21 +5,19 @@ import requests
 from requests.exceptions import HTTPError
 import html
 import xml.etree.ElementTree as ET
-from bs4 import BeautifulSoup
-#from htmlparser import parseItemContent
+from htmlparser import parseItemContent
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 
 @app.route('/')
-def getContent():
+def get():
     try:
             
         response = requests.get('https://revistaautoesporte.globo.com/rss/ultimas/feed.xml')
         response.raise_for_status()
 
         parsedXml = ET.fromstring(response.content)
-        
         items = parsedXml.findall('./channel/item')
 
         response = {'feed':[]}
@@ -29,58 +27,21 @@ def getContent():
             nItem = {}
 
             ContentMarkup = item.find('description').text
+            parser = parseItemContent()
+            parser.feed(html.unescape(ContentMarkup))
 
-            # parseando o HTML do conteúdo com biblioteca externa
-            soup = BeautifulSoup(ContentMarkup, 'html.parser')
-
-            for tag in soup.find_all(True):
-                #print(type(tag))
-                #print(tag.name)
-                if(tag.name == 'p'):
-                    #print(type(tag.string))
-                    #print(tag.contents)
-                    print(str(tag))
-                elif(tag.name == 'img'):
-                    if(tag.find_parent("div")):
-                        print(tag['src'])
-                elif(tag.name)
-            
-            # paragraphs = [str(p) for p in soup.find_all('p')]
-            # images = [img['src'] for img in soup.select("div.foto > img")]
-            # links = [link['href'] for link in soup.select("ul li > a")]
-
-            paragraphs = ' '.join(paragraphs)
-
-            description = []
-
-            nItem['title'] = item.find('title').text
+            nItem['description'] = parser.content
             nItem['link'] = item.find('link').text
-            nItem['description'] = [
-                {
-                    'type': 'text',
-                    'content' : paragraphs
-                },
-                {
-                    'type': 'image',
-                    'content' : images
-                },
-                {
-                    'type': 'links',
-                    'content': links
-                }
-            ]
+            nItem['title'] = item.find('title').text
 
             response['feed'].append({'item': nItem })
 
         return jsonify(response)
     
     except HTTPError as http_err:
-        print('Cool Exception')
         print(f'HTTP error occurred: {http_err}')
     except Exception as err:
-        print('Regular Exception')
         print(err)
  
 if __name__ == '__main__':
-    #app.run(debug=True, host='0.0.0.0')
-    print(getContent())
+    app.run(debug=True, host='0.0.0.0')
